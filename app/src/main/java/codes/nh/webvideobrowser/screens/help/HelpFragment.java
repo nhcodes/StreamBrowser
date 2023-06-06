33968b9aa2151b2406b1cf886051af8c990ea446
nhcodes <nhdotcodes@gmail.com>
2023-06-06 19:01:02 +0200

    help fragment -> coming soon

--- app/src/main/java/codes/nh/webvideobrowser/screens/help/HelpFragment.java
+++ app/src/main/java/codes/nh/webvideobrowser/screens/help/HelpFragment.java
@@ -1,13 +1,13 @@
 package codes.nh.webvideobrowser.screens.help;
 
 import android.os.Bundle;
-import android.text.Html;
 import android.text.Spanned;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.text.HtmlCompat;
 
 import codes.nh.webvideobrowser.R;
 import codes.nh.webvideobrowser.screens.sheet.SheetFragment;
@@ -24,7 +24,7 @@ public class HelpFragment extends SheetFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        Spanned html = Html.fromHtml(getString(R.string.help_html));
+        Spanned html = HtmlCompat.fromHtml(getString(R.string.help_html), HtmlCompat.FROM_HTML_MODE_LEGACY);
 
         TextView helpText = view.findViewById(R.id.fragment_help_text);
         helpText.setText(html);
